11f239ffed5371d059228e234cb65b151b18afeb
florentmuqolli <muqolliflorentt@gmail.com>
2025-05-04 01:18:43 +0200

    setting up backend

---
+++ studentmanager-backend/middleware/roleMiddleware.js
@@ -0,0 +1,11 @@
+const roleMiddleware = (...allowedRoles) => {
+    return (req, res, next) => {
+      if (!req.user || !allowedRoles.includes(req.user.role)) {
+        return res.status(403).json({ error: 'Forbidden - Insufficient role' });
+      }
+      next();
+    };
+  };
+  
+  module.exports = roleMiddleware;
+  
