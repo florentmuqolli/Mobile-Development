89a42da9d374fc86ee96e40b5ba086e3524034b5
florentmuqolli <muqolliflorentt@gmail.com>
2025-05-04 12:14:32 +0200

    added a new model (TASK) and tested it's endpoints

---
+++ studentmanager-backend/routes/taskRoutes.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+const taskController = require('../controllers/taskController');
+const authMiddleware = require('../middleware/authMiddleware');
+const roleMiddleware = require('../middleware/roleMiddleware');
+
+router.use(authMiddleware);
+
+router.post('/', roleMiddleware('admin', 'teacher'), taskController.createTask);
+router.get('/', taskController.getAllTasks);
+router.get('/:id', taskController.getTaskById);
+router.put('/:id', roleMiddleware('admin', 'teacher'), taskController.updateTask);
+router.delete('/:id', roleMiddleware(['admin']), taskController.deleteTask);
+
+module.exports = router;
--- studentmanager-backend/server.js
+++ studentmanager-backend/server.js
@@ -5,6 +5,7 @@ const cors = require('cors');
 const connectDB = require('./config/db');
 const cookieParser = require('cookie-parser');
 const authRoutes = require('./routes/authRoutes');
+const taskRoutes = require('./routes/taskRoutes');
 
 dotenv.config();
 
@@ -15,6 +16,7 @@ app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 app.use('/api/auth', authRoutes);
+app.use('/api/tasks', taskRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
